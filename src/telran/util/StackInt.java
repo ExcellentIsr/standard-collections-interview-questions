70ad78bdd5d430164d8c4e36d25e17ea7a616a3e
Danil Katz <i_like_it74@mail.ru>
2023-03-25 02:31:16 +0300

    update HW 30 DigitStatistics + StackInt + Tests

--- src/telran/util/StackInt.java
+++ src/telran/util/StackInt.java
@@ -5,37 +5,40 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class StackInt {
-	
+
 	LinkedList<Integer> list = new LinkedList<Integer>();
 	Integer max = null;
-	//complexity of all methods [1]
+
+	// complexity of all methods [1]
 	public void push(int num) {
-		if(!list.isEmpty() && max < num || list.isEmpty()) {
+		if (!list.isEmpty() && max < num || list.isEmpty()) {
 			max = num;
 		}
 		list.addFirst(num);
 	}
-	
+
 	public int pop() {
-		//return a number from a top of stack or throw noSuchELementException
-		//if stack is empty
-		if (list.isEmpty()) throw new NoSuchElementException();
+		// return a number from a top of stack or throw noSuchELementException
+		// if stack is empty
+		if (list.isEmpty())
+			throw new NoSuchElementException();
 		int res = list.removeFirst();
-		if(!list.isEmpty() && res == max) {
+		if (!list.isEmpty() && res == max) {
 			max = list.getFirst();
 		}
 		return res;
 	}
-	
+
 	public boolean isEmpty() {
-		//returns true if the stack is empty, otherwise false
+		// returns true if the stack is empty, otherwise false
 		return list.isEmpty();
 	}
-	
-	public int getMax() throws Exception{
-		//return maximal value of the stack or throw noSuchELementException
-		//if stack is empty
-		if (list.isEmpty()) throw new NoSuchElementException();
+
+	public int getMax() throws Exception {
+		// return maximal value of the stack or throw noSuchELementException
+		// if stack is empty
+		if (list.isEmpty())
+			throw new NoSuchElementException();
 		return max;
 	}
 }
