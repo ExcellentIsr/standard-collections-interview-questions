eb7b8d52ae57cd2fa5eb0c747a96b42fcd0d2ccc
Danil Katz <i_like_it74@mail.ru>
2023-03-25 02:28:56 +0300

    HW 33 DigitStatistics + StackInt + Tests

---
+++ src/telran/test/StackIntTest.java
@@ -0,0 +1,70 @@
+package telran.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import telran.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StackIntTest {
+	StackInt st = new StackInt();
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	void isEmptyTest() {
+		assertTrue(st.isEmpty());
+		st.push(10);
+		assertFalse(st.isEmpty());
+		st.pop();
+		assertTrue(st.isEmpty());
+	}
+	
+	@Test
+	void pushTest() {
+		st.push(5);
+		assertEquals(5, st.pop());
+		st.push(10);
+		assertEquals(10, st.pop());
+		st.push(2);
+		assertEquals(2, st.pop());
+		st.push(0);
+		assertEquals(0, st.pop());
+	}
+	
+	@Test
+	void popTest() {
+		st.push(5);
+		st.push(10);
+		st.push(2);
+		st.push(0);
+		assertEquals(0, st.pop());
+		assertEquals(2, st.pop());
+		assertEquals(10, st.pop());
+		assertEquals(5, st.pop());
+	}
+	
+	@Test
+	void getMaxTest() throws Exception {
+		st.push(4);
+		assertEquals(4, st.getMax());
+		st.push(5);
+		assertEquals(5, st.getMax());
+		st.push(10);
+		assertEquals(10, st.getMax());
+		st.push(2);
+		assertEquals(10, st.getMax());
+		
+		assertEquals(2, st.pop());
+		assertEquals(10, st.getMax());
+		assertEquals(10, st.pop());
+		assertEquals(5, st.getMax());
+		assertEquals(5, st.pop());
+		assertEquals(4, st.getMax());
+		assertEquals(4, st.pop());
+	}
+}
---
+++ src/telran/test/StandartCollectionTest.java
@@ -0,0 +1,34 @@
+package telran.test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StandartCollectionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	void displayoccurrencesCount() {
+		String[] strings = {"lmn", "abc", "abc", "lmn", "a", "lmn"};
+		Arrays.stream(strings).collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+				.entrySet().stream().sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+				.forEach(e -> System.out.printf("%s: %d \n", e.getKey(), e.getValue()));
+	}
+
+	@Test
+	void displayDigitStatistics() {
+		int countNumbers = 1_000_000, from = 1, to = Integer.MAX_VALUE - 1;
+		
+		new Random().ints(countNumbers, from, to).flatMap(n -> Integer.toString(n).chars()).boxed()
+			.collect(Collectors.groupingBy(digit -> digit, Collectors.counting())).entrySet()
+			.stream().sorted((e1, e2) -> Long.compare(e1.getKey(), e2.getKey()))
+			.forEach(d -> System.out.printf("%d is %d\n", d.getKey() - 48, d.getValue()));
+	}
+}
